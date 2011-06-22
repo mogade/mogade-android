145c239aa2a48806dd4e0dd54b9807cd1125e811
Darren Kopp <darrenkopp@gmail.com>
2011-06-21 22:19:00 -0600

    implemented building and execution of get requests

--- src/com/mogade/impl/DefaultCommunicator.java
+++ src/com/mogade/impl/DefaultCommunicator.java
@@ -1,29 +1,59 @@
 package com.mogade.impl;
 
-import com.mogade.ArrayResponseConverter;
-import com.mogade.ErrorMessage;
-import com.mogade.Response;
-import com.mogade.ResponseConverter;
+import android.net.Uri;
+import com.mogade.*;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.List;
 import java.util.Map;
 
 public class DefaultCommunicator {
-    public <T> Response<T> get(String endpoint, Map<String, Object> parameters, ResponseConverter<T> converter) {
+    public <T> Response<T> get(String endpoint, Map<String, Object> parameters, final ResponseConverter<T> converter) {
+        Uri.Builder builder = MogadeConfiguration.getUriBuilder();
+        builder.appendPath(endpoint);
+        addQuerystringParameters(parameters, builder);
 
-        return null;
+        HttpGet get = new HttpGet(builder.toString());
+
+        return executeRequest(get, converter);
     }
 
     public <T> Response<T> get(String endpoint, Map<String, Object> parameters, ArrayResponseConverter<T> converter) {
-        return null;
+        Uri.Builder builder = MogadeConfiguration.getUriBuilder();
+        builder.appendPath(endpoint);
+        addQuerystringParameters(parameters, builder);
+
+        HttpGet get = new HttpGet(builder.toString());
+
+        return executeRequest(get, converter);
     }
 
     public <T> Response<T> post(String endpoint, Map<String, Object> parameters, ResponseConverter<T> converter) {
         return null;
+    }
+
+    private void addQuerystringParameters(Map<String, Object> parameters, Uri.Builder builder) {
+        for (Map.Entry<String, Object> parameter : parameters.entrySet()) {
+            String key = parameter.getKey();
+            Object val = parameter.getValue();
+
+            if (val == null) continue;
+            if (val instanceof Boolean) {
+                builder.appendQueryParameter(key, Boolean.toString((Boolean) val));
+            } else if (val instanceof List) {
+                List<String> items = (List<String>) val;
+                for (String item : items) {
+                    builder.appendQueryParameter(key, item);
+                }
+            } else {
+                builder.appendQueryParameter(key, val.toString());
+            }
+        }
     }
 
     private <T> Response<T> executeRequest(HttpUriRequest request, ArrayResponseConverter<T> converter) {
